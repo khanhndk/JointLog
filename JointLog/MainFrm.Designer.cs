dae67c0fec4c12b7806f3754690f9c9cdb48756f
khanhndk <khanhndk>
2016-08-29 14:03:25 +1000

    Init

---
+++ JointLog/MainFrm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace JointLogFile
+{
+    partial class MainFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.lstFileNames = new System.Windows.Forms.ListBox();
+            this.btnJoint = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(12, 12);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(332, 20);
+            this.txtFileName.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(350, 10);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(82, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // lstFileNames
+            // 
+            this.lstFileNames.FormattingEnabled = true;
+            this.lstFileNames.Location = new System.Drawing.Point(12, 38);
+            this.lstFileNames.Name = "lstFileNames";
+            this.lstFileNames.Size = new System.Drawing.Size(332, 160);
+            this.lstFileNames.TabIndex = 2;
+            // 
+            // btnJoint
+            // 
+            this.btnJoint.Location = new System.Drawing.Point(350, 39);
+            this.btnJoint.Name = "btnJoint";
+            this.btnJoint.Size = new System.Drawing.Size(82, 23);
+            this.btnJoint.TabIndex = 3;
+            this.btnJoint.Text = "Joint";
+            this.btnJoint.UseVisualStyleBackColor = true;
+            this.btnJoint.Click += new System.EventHandler(this.btnJoint_Click);
+            // 
+            // MainFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 204);
+            this.Controls.Add(this.btnJoint);
+            this.Controls.Add(this.lstFileNames);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtFileName);
+            this.Name = "MainFrm";
+            this.Text = "Joint Log File";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.ListBox lstFileNames;
+        private System.Windows.Forms.Button btnJoint;
+    }
+}
+
---
+++ JointLog/MainFrm.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace JointLogFile
+{
+    public partial class MainFrm : Form
+    {
+        string template;
+        string ext;
+        string path;
+        public MainFrm()
+        {
+            InitializeComponent();
+        }
+
+        private static int compare_filename(string x, string y)
+        {
+            string xfilename = Path.GetFileNameWithoutExtension(x);
+            string yfilename = Path.GetFileNameWithoutExtension(y);
+            int xx = Int32.Parse(xfilename.Split(new char[] { '@' })[1]);
+            int yy = Int32.Parse(yfilename.Split(new char[] { '@' })[1]);
+            return (xx - yy);
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if(dlg.ShowDialog() == DialogResult.OK)
+            {
+                txtFileName.Text = dlg.FileName;
+                template = Path.GetFileName(txtFileName.Text).Split(new char[] { '@' })[0];
+                ext = Path.GetExtension(txtFileName.Text);
+                path = Path.GetDirectoryName(txtFileName.Text);
+                string[] files = Directory.GetFiles(path, template + "@*" + ext);
+                Array.Sort<string>(files, compare_filename);
+                foreach(string filename in files)
+                {
+                    lstFileNames.Items.Add(filename);
+                }
+            }
+        }
+
+        private string get_header(string filename)
+        {
+            StreamReader reader = new StreamReader(filename);
+            string line = reader.ReadLine();
+            string[] items = line.Split(new char[] { '\t' });
+            string result = "";
+            foreach(string item in items)
+            {
+                string name = item.Split(new char[] { ':' })[0];
+                result += name + "#1," + name + ",";
+            }
+            return result;
+        }
+
+        private void btnJoint_Click(object sender, EventArgs e)
+        {
+            btnJoint.Enabled = false;
+            StreamWriter writer = new StreamWriter(path + "\\" + template + ext);
+            string header = get_header(lstFileNames.Items[0].ToString());
+            writer.WriteLine(header);
+            for(int i = 0; i < lstFileNames.Items.Count; i++)
+            {
+                string filename = (string)lstFileNames.Items[i];
+                StreamReader reader = new StreamReader(filename);
+                while (!reader.EndOfStream)
+                    writer.WriteLine(reader.ReadLine().Replace('\t',',').Replace(':',','));
+                reader.Close();
+            }
+            writer.Close();
+            btnJoint.Enabled = true;
+        }
+    }
+}
